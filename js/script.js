e98459af62801c28262210ae709c48c83670bc62
No NOréo <70480609+Non0reo@users.noreply.github.com>
2022-02-16 19:50:11 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -46,6 +46,15 @@ function UpdateTitle(pageState){
             break;
 
         case 1:
+            headerIcon.style = "transform:translate(-100%);";
+            headerText.style = "transform: translate(-10%);";
+            headerText.innerHTML = "La Biodiversité en Perpetuelle Evolution";
+            color("#494949");
+
+            leftArrow.style = "transform:translate(0%);"
+            break;
+
+        case 2:
             headerIcon.src = 'css/img/title/one.png';
             headerIcon.style = "transform:translate(0%);";
             headerText.style = "transform: translate(0%);";
@@ -55,14 +64,14 @@ function UpdateTitle(pageState){
             leftArrow.style = "transform:translate(0%);";
             break;
 
-        case 2:
+        case 3:
             headerIcon.src = 'css/img/title/two.png';
             headerText.innerHTML = "La biodiversité des espèces et des écosystèmes";
             color("#5374ce");
 
             break;
 
-        case 3:
+        case 4:
             headerIcon.src = 'css/img/title/three.png';
             headerIcon.style = "transform:translate(0%);";
             headerText.style = "transform: translate(0%);";
@@ -72,7 +81,7 @@ function UpdateTitle(pageState){
             rightArrow.style = "transform:translate(0%);";
             break;
 
-        case 4:
+        case 5:
             headerIcon.style = "transform:translate(-100%);";
             headerText.style = "transform: translate(-10%);";
             headerText.innerHTML = "Fin";
@@ -84,18 +93,22 @@ function UpdateTitle(pageState){
 }
 
 function UpdateSlide(pageState){
-    for (let i = 0; i < mainTextSlide.length; i++) {
-
         switch(pageState){
             case 0:
-                mainTextSlide[i].innerHTML = "Hellow World"; 
+                displayZone.innerHTML = "<p class=\"mainText\">Hello, World!</p>";
                 break;
 
             case 1:
-                mainTextSlide[i].innerHTML = "Salut tout le monde"; 
+                displayZone.innerHTML = "<p class=\"mainText\">   La nature apporte depuis des milliards d\'années tout un panel d\’espèces différentes ayant chacune leur propre particularités physiques. Chaque espèce évolue chacun de son côté, normalement indépendamment des autres. Mais le développement d’une espèce est beaucoup plus complexe que ça car il dépend aussi des autres espèces et du milieu.<br><br>   Dans ce petit exposé numérique, nous verrons comment une espèce persiste dans le temps et les particularités qu\'elle reçoit en fonction du milieu dans lequel elle évolue et en fonction des gènes qu’ils lui sont transmis ; dans quel espace et milieu les animaux évoluent ; et nous verrons comment la survie d’une espèce dépend de celle des autres ou de son environement.</p>"; 
                 break;
+            
+            case 2:
+                displayZone.innerHTML = "<p class=\"mainText\">→ On a comparé les caractéristiques de 3 personnes de notre entourage, pour ainsi expliquer comment se fait la biodiversité génétique.</p><table><tr><th>Caractéristiques</th><th>Individus 1</th><th>Individus 2</th><th>Individus 3</th></tr><tr><td>Couleur des yeux</td><td>Vert</td><td>Bleu</td><td>Marron</td></tr><tr><td>Couleur des cheveux</td><td>Brun</td><td>Blond</td><td>Châtin</td></tr><tr><td>Sexe</td><td>Homme</td><td>Femme</td><td>Femme</td></tr><tr><td>Groupe Sanguin</td><td>O+</td><td>A-</td><td>A+</td></tr></table><p class=\"mainText\"> On observe ici une biodiversité génétique car, toutes ces variations de l’être humain se font en fonction des gènes hérités de nos parents grâce au hasard.</p>";
+                break;
+            
+            case 3:
+                displayZone.innerHTML = "<p class=\"mainText\">pouet</p>"
         }
-    }
 }
 
 
